06f8befab070114c7840a6e4cc2ebbdb8396cf96
Yunus <yunusozdemir468@gmail.com>
2021-09-20 20:11:54 +0300

    comment line added

--- Algorithm/Algorithm/Program.cs
+++ Algorithm/Algorithm/Program.cs
@@ -657,25 +657,25 @@ namespace Algorithm
             #endregion
 
             #region Girilen texti arraya çevirip belirlenen kelimeleri çekmesi
-            string value="";
-            Console.WriteLine("Texti giriniz");
-            string line= Console.ReadLine();
-            Console.WriteLine("Seçilecek karakterleri giriniz");
-            string words =Console.ReadLine();
-            for (int i = 0; i < line.Length; i++)
-            {
-                for (int j = 0; j < words.Length; j++)
-                {
-                    if (line[i]==words[j])
-                    {
-                        if (!value.Contains(line[i]))
-                        {
-                            value += line[i];
-                        }
-                    }
-                }
-            }
-            Console.WriteLine(value);
+            //string value = "";
+            //Console.WriteLine("Texti giriniz");
+            //string line = Console.ReadLine();
+            //Console.WriteLine("Seçilecek karakterleri giriniz");
+            //string words = Console.ReadLine();
+            //for (int i = 0; i < line.Length; i++)
+            //{
+            //    for (int j = 0; j < words.Length; j++)
+            //    {
+            //        if (line[i] == words[j])
+            //        {
+            //            if (!value.Contains(line[i]))
+            //            {
+            //                value += line[i];
+            //            }
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(value);
             #endregion
         }
 
