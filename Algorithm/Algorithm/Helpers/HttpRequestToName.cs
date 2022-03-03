6dcc40dc18b95d6fecfefbde89a1f1a37185b8cd
Yunus <yunusozdemir468@gmail.com>
2022-03-04 00:16:17 +0300

    Create HttpRequestToName.cs

---
+++ Algorithm/Algorithm/Helpers/HttpRequestToName.cs
@@ -0,0 +1,56 @@
+namespace Algorithm.Helpers
+{
+    public static class HttpRequestToName
+    {
+        public static void HttpRequestToName()
+        {
+            // ReportModel model = new ReportModel();
+
+
+            //            var baseAddress = "https://api.abc.com";
+
+            // var myApi = RestService.For<IMemberApi>(baseAddress);
+            // var result = myApi.Authentication(new T_Member { Member_Email = "asd@gmail.com", Member_Password = "asd123." }).Result;
+
+
+
+            // var api = RestService.For<IMemberApi>(baseAddress, new RefitSettings()
+            // {
+            //     AuthorizationHeaderValueGetter = () => Task.FromResult(result.token)
+            // });
+
+            // var memberCount = api.GetMemberCountAsync().Result;
+
+            // model.memberCount = memberCount;
+
+
+
+            // List<SelectListItem> yearSelectList = new List<SelectListItem>();
+            // List<SelectListItem> monthSelectList = new List<SelectListItem>();
+
+            // int i = DateTime.Now.Year;
+
+            // while (i >= 2020)
+            // {
+            //     yearSelectList.Add(new SelectListItem() { Text = i.ToString(), Value = i.ToString() });
+            //     i--;
+            // }
+
+            // int y = 1;
+
+            // while (y <= 12)
+            // {
+            //     monthSelectList.Add(new SelectListItem() { Text = y.ToString(), Value = y.ToString() });
+            //     y++;
+            // }
+
+
+            // model.YearList = yearSelectList;
+
+            // model.MonthList = monthSelectList;
+
+
+            // return View(model);
+        }
+    }
+}
